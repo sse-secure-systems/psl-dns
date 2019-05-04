4b6b84445e7f800c8ac91e38f4bd2817971a4d61
Peter Thomassen <peter.thomassen@securesystems.de>
2019-05-04 21:35:44 +0200

    bump version to 1.0rc1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="psl-dns",
-    version="1.0b3",
+    version="1.0rc1",
     author="Peter Thomassen",
     author_email="mail@peter-thomassen.de",
     description="Query the Public Suffix List (PSL) via DNS and check the PSL status of a domain.",
@@ -15,7 +15,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     install_requires=['dnspython>=1.14.0'],
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
