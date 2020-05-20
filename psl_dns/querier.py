e6beb92f70852511755ef8e0290b97a7133db8b1
Peter Thomassen <peter.thomassen@securesystems.de>
2020-05-20 15:21:33 +0200

    compare full string against public suffx, not just number of labels

--- psl_dns/querier.py
+++ psl_dns/querier.py
@@ -83,7 +83,7 @@ class PSL(PSLBase):
 
     def is_public_suffix(self, domain, public_suffix=None):
         public_suffix = public_suffix or self.get_public_suffix(domain)
-        return domain.count('.') == public_suffix.count('.')
+        return domain == public_suffix
 
     def query(self, domain, rdatatype):
         # Normalize, then construct QNAME, and retrieve response
